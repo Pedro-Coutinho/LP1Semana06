274aa8f3bd3cf76d90fd51e29a40095fec61910d
Pedro Coutinho <pedro_coutinho18@outlook.com>
2023-04-21 10:41:01 +0100

    Ex 5

--- GameSix/Program.cs
+++ GameSix/Program.cs
@@ -6,7 +6,30 @@ namespace GameSix
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Give me the number of enemies:");
+            int nEnemies = 0;
+            bool validInput = false;
+            do
+            {
+                validInput = Int32.TryParse(Console.ReadLine(), out nEnemies);
+                if(!validInput) Console.WriteLine("Invalid input!");
+            }
+            while (!validInput);
+
+            Foe[] enemies = new Foe[nEnemies];
+            for(int i = 0; i< nEnemies; i++)
+            {
+                Console.WriteLine("Enemie name?");
+                string name = Console.ReadLine();
+                enemies[i] = new Foe(name);
+            }  
+
+            foreach(Foe foe in enemies)
+            {
+                string n = foe.GetName();
+                Console.WriteLine(n);
+            }
+        
         }
     }
 }
