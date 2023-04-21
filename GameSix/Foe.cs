44531f6ab8c7333c90784aee76e2af8271bbf8d5
Pedro Coutinho <pedro_coutinho18@outlook.com>
2023-04-21 10:21:13 +0100

    Ex 3

--- GameSix/Foe.cs
+++ GameSix/Foe.cs
@@ -9,7 +9,21 @@ namespace GameSix
             this.name = name;
             health = 100;
             shield = 0;
-            
+
+        }
+
+        public string GetName() => name;
+
+        public void TakeDamge(float damage)
+        {
+            shield -= damage;
+            if (shield < 0)
+            {
+                float damageStillToInflict = -shield;
+                shield = 0;
+                health -= damageStillToInflict;
+                if(health < 0) health = 0;
+            }
         }
     }
 }
